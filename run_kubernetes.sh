49a8f7c039f5a3b84e638548078b3cf70f09a22b
abdo2017 <abdoesam14@gmail.com>
2021-12-04 09:30:55 +0200

    finsih

---
+++ run_kubernetes.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# This tags and uploads an image to Docker Hub
+
+# Step 1:
+# This is your Docker ID/path
+# dockerpath=<>
+
+dockerpath="abdoesam2011/abdo"
+
+# Step 2
+# Run the Docker Hub container with kubernetes
+kubectl run abdoservice --image=$dockerpath --port=80
+
+# Step 3:
+# List kubernetes pods
+kubectl get pods
+# Step 4:
+# Forward the container port to a host
+kubectl port-forward abdoservice 8000:80 
